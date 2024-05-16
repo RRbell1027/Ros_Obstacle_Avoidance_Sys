dafab211e7863cb2b7cd27024c64c4dedd00c1d3
RRbell1027 <jack345354@gmail.com>
2024-05-16 22:13:34 +0800

    Relocate all the file and add sensor
    To fit the last commmit that change the way I use dockerfile from
    "copy all the folder into container" to "mount this folder", I
    move all the package folders into `src/`.
    
    Create the new package "my_sensor" which will publish reality data.
    The first sensor I use is zed camera, it can send position and
    orientation back, that sound amazing for me to implement slam in
    real world.
    
    In the `camera_node.py` file, I receive position data by tcp/ip
    because zed sdk depends on cuda which rasberry pi 4 doesn't has. So
    camera will work on other cuda supported device such as laptop.
    
    I try to simulate lidar slam by gazebo but I forgot to add install
    command into dockerfile.

---
+++ src/my_sensor/my_sensor/camera_node.py
@@ -0,0 +1,108 @@
+import sys, socket
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PointStamped
+
+def socket_init(ip, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.settimeout(0.1)
+    server_socket.listen(1)
+    return server_socket
+
+def socket_connect(server_socket):
+    # wait for connect
+    client_socket, client_address = server_socket.accept()
+    print(f'client socket: {client_socket}, address: {client_address}')
+
+    return client_socket
+
+def socket_recv(client_socket):
+    # wait for camera distance
+    data = client_socket.recv(1024)
+    if data:
+        position = data.decode().split('/')[-2]
+        print(f'position = {position}')
+        return position
+    print('get nothing')
+
+def process(str_position):
+    list_pos = str_position.split(' ')
+    return list_pos[0], list_pos[1], list_pos[2]
+
+
+class CameraNode(Node):
+
+    def __init__(self):
+        super().__init__('camera_node')      
+        # publish data from camera
+        self.publisher_ = self.create_publisher(PointStamped, 'camera_position', 10)
+
+
+def main(args=None):
+
+    # get args
+    if args == None or len(args) == 1:
+        print('The node needs socket ip and port')
+        return -1
+        
+    ip, port = args[0], args[1]
+
+    #init server socket
+    server_socket = socket_init(ip, port)
+
+    # init Node
+    rclpy.init(args=args)
+    node = CameraNode()
+
+    stop = False
+    while not stop:
+        client_socket = None
+        try:
+            # connect camera and host
+            print('wait for connect...')
+            # client_socket = socket_connect(server_socket)
+            client_socket, client_address = server_socket.accept()
+            print(f'client socket: {client_socket}, address: {client_address}')
+        except TimeoutError:
+            continue
+        except Exception as e:
+            print(f'An error occurred: {e}')
+            break
+            
+        while True:
+            try:
+                # tcp wait recieve distance data
+                position = socket_recv(client_socket)
+                if not position:
+                    print("distance not found, disconnected.")
+                    client_socket.close()
+                    break
+
+                # processs position
+                tx, ty, tz = process(position)
+                
+                # ros publish
+                message = PointStamped()
+                message.header.frame_id = 'odom'
+                message.point.x = tx
+                message.point.y = ty
+                message.point.z = tz
+                node.publisher_.publish(message)
+            except KeyboardInterrupt as e:
+                print(f'An error occurred: {e}')
+                stop = True
+                break
+            except:
+                print(f'An error occurred: {e}')
+                break
+
+
+    node.destroy_node()
+
+    if server_socket:
+        server_socket.close()
+
+if __name__ == '__main__':
+    main(sys.argv)
+
